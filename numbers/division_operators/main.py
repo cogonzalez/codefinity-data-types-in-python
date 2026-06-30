ffd4ba458223a15bfd4f5e7b2ede9c7d70d0a160
cogonzalez <>
2026-06-30 15:58:53 +0000

    Division Operators

--- numbers/division_operators/main.py
+++ numbers/division_operators/main.py
@@ -1,8 +1,8 @@
 # Calculate the number of completed transactions
-completed = ___
+completed = 60 // 7
 # Calculate the number of remaining minutes
-minutes = ___
+minutes = 60 % 7
 
 # Print these values
-print("The number of completed transactions is", ___)
-print("The number of remaining minutes is", ___)
+print("The number of completed transactions is", completed)
+print("The number of remaining minutes is", minutes)
